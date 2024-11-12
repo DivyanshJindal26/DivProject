1b14bcc993fc0261b3931a1eca771723dc551799
Divyansh Jindal <divyanshjindal95.2012@gmail.com>
2024-11-12 23:28:00 +0530

    update app.py

--- Website/app.py
+++ Website/app.py
@@ -30,4 +30,4 @@ def chatbot():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=False)  # debug=True for development; remove for production
+    app.run(debug=False, host='0.0.0.0', port=5000)  # debug=True for development; remove for production
